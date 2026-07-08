bf882b87a4237e92d26d61d8bb447e5941eff2d8
DarynaHyena <daryna.novokhatska@gmail.com>
2026-07-08 21:06:00 +0300

    summary

---
+++ booking/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from booking import views
+
+
+urlpatterns = [
+  path('houses/', views.houses_list_view, name='houses-list'),
+  path('houses/detail/<int:pk>', views.house_detail_view, name='house-detail'),
+  path('houses/create/', views.house_create_view, name='house-create'),
+  path('houses/update/<int:pk>', views.house_update_view, name='house-update'),
+  path('houses/delete/<int:pk>', views.house_delete_view, name='house-delete'),
+]
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from booking.models import HouseModel, BookingModel
 
-# Create your views here.
+
+def houses_list_view(request):
+  houses = HouseModel.objects.all()
+  context = {
+    "houses": houses
+  }
+  return render(
+    request, 
+    'houses/list.html', 
+    context
+    )
+
+def house_detail_view(request, pk):
+  house = get_object_or_404(HouseModel, pk=pk)
+  context = {
+    "house": house
+  }
+  return render(
+    request, 
+    'houses/detail.html', 
+    context
+    )
+
+def house_create_view(request):
+  pass
+
+def house_update_view(request):
+  pass
+
+def house_delete_view(request):
+  pass
